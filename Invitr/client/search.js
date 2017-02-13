f35ecab650a5cdd8dfe71daf45be6b4711925c8c
nicknguyenweb <nicknguyen41@yahoo.com>
2017-02-12 18:02:08 -0600

    Search functionality part 2

--- Invitr/client/search.js
+++
@@ -1,5 +0,0 @@
-import { Index, MinimongoEngine} from 'meteor/easy:search';
-
-Template.search.helpers({
-   usersIndex: () => UsersIndex,
-})
--- Invitr/lib/search.js
+++
@@ -1,6 +0,0 @@
-const Users = new Mongo.Collection('users')
-const UsersIndex = new Index({
-   collection: Users,
-   fields: ['name'],
-   engine: new MinimongoEngine(),
-})

1fff96d86c1059cb054e4e32ee0129ccd15c3b25
khong756 <khong756@gmail.com>
2017-03-05 22:46:18 -0600

    aesthetic changes

--- Invitr/client/homePage.js
+++ Invitr/client/homePage.js
@@ -1,25 +1,39 @@
-import { Events } from '../collections/event.js';
-import { UserProfile } from '../collections/event.js';
+import {
+    Events
+} from '../collections/event.js';
+import {
+    UserProfile
+} from '../collections/event.js';
 Meteor.subscribe('allUsers');
 Template.homePage.events({
-  'click .searchButton'(event) {
-    var textField = $('#searchBox').val();
-    
-    FlowRouter.go('/search/' + textField);
-  },
+    'click .searchButton' (event) {
+        var textField = $('#searchBox').val();
+
+        FlowRouter.go('/search/' + textField);
+    },
 });
 
 Template.homePage.helpers({
     eventsHelper() {
-        
-          
-      var events = Events.find({date: {$gte: new Date()}}, {sort: {date: 1}}).fetch();
-    for (event of events){
-        var user = Meteor.users.findOne({"_id": event.host})
-        console.log(user);
-        event.host = user.profile.firstname + " " + user.profile.lastname + " (" + user.username + ") " 
-    }
-      console.log('it worked');
-      return events;
+
+
+        var events = Events.find({
+            date: {
+                $gte: new Date()
+            }
+        }, {
+            sort: {
+                date: 1
+            }
+        }).fetch();
+        for (event of events) {
+            var user = Meteor.users.findOne({
+                "_id": event.host
+            })
+            console.log(user);
+            event.host = user.profile.firstname + " " + user.profile.lastname + " (" + user.username + ") "
+        }
+        console.log('it worked');
+        return events;
     },
 });
